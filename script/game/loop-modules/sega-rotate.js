c8fbda997e1004b10102232051d9ef440d343e47
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-14 16:34:20 +0800

    Update sega-rotate.js

--- script/game/loop-modules/sega-rotate.js
+++ script/game/loop-modules/sega-rotate.js
@@ -1,11 +1,38 @@
 import input from "../../input.js"
+import {
+  PIECES,
+  MONOMINO_PIECES,
+  DOMINO_PIECES,
+  TROMINO_PIECES,
+  PENTOMINO_PIECES,
+  SPAWN_OFFSETS,
+  KICK_TABLES,
+  INITIAL_ORIENTATION,
+  PIECE_OFFSETS,
+  SPIN_POINTS,
+} from "../consts.js"
 
 export default function segaRotate(arg) {
-  const piece = arg.piece
-  if (piece.y + 1 < 4 && piece.shape === "I") {
+  /*if (piece.y + 1 < 4 && piece.shape === "I") {
 	  return
   } else if (piece.y + 1 < 2) {
 	  return
+  }*/
+  const piece = arg.piece
+  let rotationSystem = this.parent.rotationSystem
+  let pieceShape = piece.shape
+  let downShift = SPAWN_OFFSETS[rotationSystem][downShift]
+  let yShift = SPAWN_OFFSETS[rotationSystem][pieceShape][1]
+  let spawnY = downShift - yShift
+  let pieceY = piece.y + downShift - yShift
+  if (pieceShape === "I") {
+	  if (pieceY <= spawnY + 2) {
+		  return
+	  }
+  } else {
+	  if (pieceY <= spawnY) {
+		  return
+	  }
   }
   if (input.getGamePress("rotateLeft")) {
     piece.rotateLeft()
